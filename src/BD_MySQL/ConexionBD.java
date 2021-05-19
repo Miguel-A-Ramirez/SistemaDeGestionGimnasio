83109289dadecc0e666248d1d56758e013ce75f0
Miguel Angel Ramirez <84107030+Miguel-A-Ramirez@users.noreply.github.com>
2021-05-18 21:18:12 -0500

    ConexionBD corregido

---
+++ src/BD_MySQL/ConexionBD.java
@@ -0,0 +1,54 @@
+//Paquete principal
+package BD_MySQL;
+
+//Importacion de librerias SQL
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Miguel Angel Ramirez Rodriguez
+ */
+
+//Clase principal
+public class ConexionBD {
+
+    // Librería de MySQL
+    public String driver = "com.mysql.jdbc.Driver";
+
+    // Nombre de la base de datos
+    public String database = "gimnasio";
+
+    // Host
+    public String hostname = "www.suhostname.com";
+
+    // Puerto
+    public String port = "3306";
+
+    // Ruta de nuestra base de datos (desactivamos el uso de SSL con "?useSSL=false")
+    public String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=false";
+
+    // Nombre de usuario
+    public String username = "user";
+
+    // Clave de usuario
+    public String password = "root";
+
+    //Funcion que conecta la BD con el programa
+    public Connection connect() {
+        Connection conexionBD = null;
+
+        try {
+            Class.forName(driver);
+            conexionBD = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conexionBD;
+    }
+    
+    
+
+}
